8a1ab9a03ab9a6b2153dbeb986eb94bd17ba37bc
JOHNPETER KIBET KECH <peter.kech@tracom.co.ke>
2021-03-14 19:09:49 +0300

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/kechkibet/libphonenumber
+
+go 1.12
+
+require (
+	github.com/go-flutter-desktop/go-flutter v0.42.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/ttacon/builder v0.0.0-20170518171403-c099f663e1c2 // indirect
+	github.com/ttacon/libphonenumber v1.2.0
+)
---
+++ libphonenumber.go
@@ -0,0 +1,48 @@
+package libphonenumber
+
+import (
+	"github.com/go-flutter-desktop/go-flutter"
+	"github.com/go-flutter-desktop/go-flutter/plugin"
+	"github.com/ttacon/libphonenumber"
+	"log"
+	"strings"
+)
+
+const channelName = "codeheadlabs.com/libphonenumber"
+
+type LibPhoneNumber struct{}
+
+var _ flutter.Plugin = &LibPhoneNumber{} // compile-time type check
+
+func (p *LibPhoneNumber) InitPlugin(messenger plugin.BinaryMessenger) error {
+	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
+	channel.HandleFunc("isValidPhoneNumber", handleisValidPhoneNumber)
+	channel.HandleFunc("normalizePhoneNumber", handleisNormalizePhoneNumber)
+	return nil // no error
+}
+
+func handleisValidPhoneNumber(arguments interface{}) (reply interface{}, err error) {
+	argsMap := arguments.(map[interface{}]interface{})
+	phone_number := argsMap["phone_number"].(string)
+	iso_code := argsMap["iso_code"].(string)
+
+	p, err := libphonenumber.Parse(phone_number, strings.ToUpper(iso_code))
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+	return libphonenumber.IsValidNumber(p), err
+}
+
+func handleisNormalizePhoneNumber(arguments interface{}) (reply interface{}, err error) {
+	argsMap := arguments.(map[interface{}]interface{})
+	phone_number := argsMap["phone_number"].(string)
+	iso_code := argsMap["iso_code"].(string)
+
+	p, err := libphonenumber.Parse(phone_number, strings.ToUpper(iso_code))
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+	return libphonenumber.Format(p, libphonenumber.E164), err
+}
